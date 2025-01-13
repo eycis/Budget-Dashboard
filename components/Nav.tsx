8731a48045350a7720df086e3dfd24475a5e9173
eycis <eycisxxx@gmail.com>
2025-01-13 17:24:39 +0100

    Login page visibility

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,4 +1,4 @@
-import { HomeIcon, ChartBarIcon, PlusCircleIcon, BellIcon } from '@heroicons/react/24/solid'
+import { HomeIcon, ChartBarIcon, PlusCircleIcon, BellIcon, ArrowRightOnRectangleIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -30,6 +30,13 @@ const Nav = () => {
         <Link to="/Notifications">
           <button>
             <BellIcon className="h-10 w-10" />
+          </button>
+        </Link>
+      </div>
+      <div className="nav-icons">
+        <Link to="/Login">
+          <button>
+          <UserCircleIcon className="h-10 w-10" />
           </button>
         </Link>
       </div>
