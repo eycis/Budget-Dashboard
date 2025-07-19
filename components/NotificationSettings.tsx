ece8b1a5def94bbfbf75367b889b20aa838d3865
eycis <eycisxxx@gmail.com>
2025-07-19 23:27:15 +0200

    UI changes in NotificationSetting component

--- components/NotificationSettings.tsx
+++ components/NotificationSettings.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Notification } from '@/models/notification';
 import Switch from '@mui/material/Switch/Switch';
 import ConfirmationModal from './confirmationModal';
-import DashboardNotifications from './DashboardNotifications';
 import UpcomingPayments from './charts/UpcomingPayments';
 import { getNotifications } from '@/Services/getNotificationsService';
 import { SaveNotification } from '@/Services/saveNotificationService';
@@ -61,28 +60,27 @@ const NotificationSettings = () => {
   return (
     <div className='bg-[#1c1c1e] relative w-full h-screen overflow-y-scroll p-5'>
       <p className='dashboard-main'>Notifications Settings</p>
-      <div className='grid grid-cols-2 col-span-2 gap-2'>
+      <div className='grid grid-cols-2 gap-2'>
         <div>
+        <div className='grid grid-cols-2 w-fit'>
           <p className='transactions-text'>Description of Notification</p>
           <input
             name = 'notificationDescription'
             id = "notificationDescription"
-            className='ml-10 px-5 w-3/5 h-10 rounded-2xl bg-[#2a2a2c] text-white text-lg font-title'
+            className='ml-10 px-5 w-3/5 h-fit rounded-2xl bg-[#2a2a2c] text-white text-lg font-title'
           />
-          <p className='transactions-text mt-7'>Set the amount</p>
+          <p className='transactions-text'>Set the amount</p>
           <input
             name = 'amount'
             type = 'number'
             id = "amount"
-            className='ml-10 px-5 w-1/4 h-10 rounded-2xl bg-[#2a2a2c] text-white font-title'
+            className='ml-10 px-5 w-3/5 h-fit rounded-2xl bg-[#2a2a2c] text-white font-title'
           />
-          <div className='grid grid-cols-2 w-fit'>
             <div>
-              <p className='transactions-text mt-7'>Will the transaction will be reoccuring?</p>
+              <p className='transactions-text'>Reoccuring transaction</p>
             </div>
-            <div className='mt-7'>
+            <div>
               <Switch
-                className='ml-28'
                 name = 'isReccuring'
                 id = "isRecurring"
                 checked={ocurringNotification}
@@ -92,20 +90,22 @@ const NotificationSettings = () => {
                 {ocurringNotification ? 'Ocurring' : 'One-Time-Thing'}
               </span>
             </div>
-              <div className='flex items-center space-x-4' data-aos='fade-up'>
-                <p className='transactions-text mt-7'>Date of payment</p>
+              <div className='' data-aos='fade-up'>
+                <p className='transactions-text'>Date of payment</p>
+                </div>
+                <div>
                 <input
                   type='date'
                   name= 'dueDate'
                   id = "dueDate"
-                  className='bg-[#3e3e42] rounded-2xl font-title text-white px-2 h-2/3 mt-3'
+                  className='bg-[#3e3e42] rounded-full text-white px-2 h-2/4'
                 />
               </div>
           </div>
           <button
             onClick={createNotification}
             data-aos='fade-down'
-            className='p-4 mt-7 ml-10 bg-white rounded-2xl font-title font-bold text-black text-xl hover:bg-[#3a3aa3] transition-colors duration-500'
+            className='p-3 mt-5  bg-white rounded-2xl font-title font-bold text-black hover:bg-[#3a3aa3] transition-colors duration-500'
           >
             Create Notification
           </button>
@@ -113,17 +113,12 @@ const NotificationSettings = () => {
             <ConfirmationModal message= {saveState? 'Notification saved!': 'Something went wrong!'} onClose={handleCloseModal} />
           )}
         </div>
-        <div className='grid grid-rows-2 h-[80%] gap-8 mr-8'>
-          <div className='h-full overflow-hidden overflow-y-auto rounded-2x'>
-            <DashboardNotifications />
-          </div>
-          <div className='h-full flex flex-col justify-start items-center -ml-56 '>
+          <div className='h-full justify-center items-center '>
             <p className='transactions-text'>Upcoming Payments in comparison with your Total Income</p>
             <UpcomingPayments />
           </div>
         </div>
       </div>
-    </div>
   );
 };
 
